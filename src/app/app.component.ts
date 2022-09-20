6f5d32b6d098163b4c7c5f55eeecf541085adcbc
Maciej Fratczak <mfratczak88@gmail.com>
2022-09-20 18:45:14 +0200

    [ci/cd] firebase setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'sps';
+  parking$: Observable<Parking[]>;
+
+  constructor(private afs: AngularFirestore) {
+    const parkingCollection = afs.collection<Parking>('parkings');
+    this.parking$ = parkingCollection.valueChanges();
+  }
+}
+
+interface Parking {
+  name: string;
+  address: {
+    city: string;
+    streetName: string;
+    streetNumber: string;
+  };
+  capacity: number;
+  operationHours: {
+    timeFrom: number;
+    timeTo: number;
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
